e3fb4538d9b911e50ce2b070b73818511d59c0df
ptyz030529 <429000366@qq.com>
2014-08-01 01:02:36 +0800

    add skin and fix namespace tag bug

--- src/jquery-asScrollbar.js
+++ src/jquery-asScrollbar.js
@@ -17,12 +17,16 @@
          options = this.options = $.extend({}, Plugin.defaults, options || {});
 		
 		 this.classes = {
-			contentClass: options.namespace + 'content',
-			wrapperClass: options.namespace +'wrapper',
-			barClass: options.namespace +'scrollbar',
-			handleClass: options.namespace +'handle',
-			directionClass : options.namespace + options.direction
+			contentClass: options.namespace + '-content',
+			wrapperClass: options.namespace +'-wrapper',
+			barClass: options.namespace +'-scrollbar',
+			handleClass: options.namespace +'-handle',
+			directionClass : options.namespace + '-'+options.direction,
 		 };
+
+         if(options.skin){
+            this.classes.skinClass = options.namespace + '-' + options.skin;
+         }
 
          if (this.options.direction === 'vertical') {
              oriAttr = this.oriAttr = {
@@ -79,8 +83,9 @@
          $container.css({
              'overflow': 'hidden',
              'position': 'relative'
-         });
+         }).addClass(this.classes.skinClass);
 
+        
          var wrapper = $wrapper[0];
          $wrapper.css(oriAttr.overflow, 'scroll')
              .css(oriAttr.crossSize, wrapper.parentNode[oriAttr.crossClient] + wrapper[oriAttr.crossOffset] - wrapper[oriAttr.crossClient] + 'px');
@@ -104,6 +109,7 @@
          direction: 'vertical', //if it's 0, scroll orientation is 'horizontal',else scroll orientation is 'vertical'.
          namespace: 'asScrollable',
          mousewheel: 10,
+         skin:false,
          responsive: false
      };
 
